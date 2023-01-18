4ea11c4a88bda10ad6b4fb53f6f888311dd9ec5d
imaadecima <imaadecima@gmail.com>
2023-01-18 14:10:51 -0300

    [feature] add new page for Item1

---
+++ pages/item1.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Item1 = () => {
+  return (
+    <>
+      <div>item1</div>
+      <style jsx>{`
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          min-height: 100vh;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default Item1
